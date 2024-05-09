d2d633419470ca7f37b494e8ccd8399e2e9fc96c
CXwudi <cx2010017@gmail.com>
2024-05-09 23:55:56 +0000

    remove unneeded test case

--- config_generator/test/component/llm_poper/test_groq.py
+++ config_generator/test/component/llm_poper/test_groq.py
@@ -8,7 +8,3 @@ class TestTogetherAiProvider(unittest.TestCase):
   def setUp(self) -> None:
     read_config('config.yaml')
     self.provider = GroqProvider()
-
-  def test_read_from_webpage(self):
-    model_names = self.provider.read_from_webpage()
-    print(model_names)
