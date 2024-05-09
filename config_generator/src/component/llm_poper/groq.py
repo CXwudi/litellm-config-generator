3ccfe3336727b1c04a55ec260211cd9513aae1eb
CXwudi <cx2010017@gmail.com>
2024-05-09 23:50:38 +0000

    use groq's new api to fetch model list

--- config_generator/src/component/llm_poper/groq.py
+++ config_generator/src/component/llm_poper/groq.py
@@ -1,6 +1,7 @@
 from ..abstract_llm_poper import AbstractLLMPoper
+from ...util.common_apis import list_all_model
+from ...util.configs import llm_config
 import logging
-from bs4 import BeautifulSoup
 import requests
 
 logging.basicConfig(level=logging.INFO)
@@ -10,11 +11,15 @@ class GroqProvider(AbstractLLMPoper):
 
   def __init__(self):
     self._models = None
+    self.config = llm_config()["groq"].as_dict()
 
   @property
   def models(self):
     if self._models is None:
-      self._models = self.read_from_webpage()
+      all_models = list_all_model(
+        "https://api.groq.com/openai/v1/models",
+        auth_header={"Authorization": f"Bearer {self.config["api-key"]}"})
+      self._models = all_models
     return self._models
 
   def pop_text_models(self):
@@ -22,33 +27,10 @@ class GroqProvider(AbstractLLMPoper):
     log.info("Found %s models, they are %s" % (len(list), str([model["model_name"] for model in list])))
     return list
   
-  def read_from_webpage(self):
-    raw_html = requests.get(
-      "https://console.groq.com/docs/models").text
-
-    # save the raw HTML to file for debugging
-    # with open("togetherai.html", "w") as file:
-    #   file.write(raw_html)
-    soup = BeautifulSoup(raw_html, 'html.parser')
-
-    model_sections = soup.find_all('h3')
-    models_info = []
-
-    for section in model_sections:
-        details = {}
-        ul = section.find_next('ul')
-        for li in ul.find_all('li'):
-            key = li.strong.text.strip(':')
-            value = li.code.text if li.code else li.text.split(': ')[1]
-            details[key] = value
-        models_info.append(details)
-    
-    return models_info
-
   def _template(self, model: dict) -> dict:
-    model_name = model['Model ID']
+    model_name = model['id']
     model_name_prefixed = f"groq/{model_name}"
-    context_window = model['Context Window'].split(' ')[0].replace(',', '')
+    context_window = model['context_window']
     return {
       "model_name": model_name_prefixed,
       "litellm_params": {
@@ -56,7 +38,7 @@ class GroqProvider(AbstractLLMPoper):
         "api_key": "os.environ/GROQ_API_KEY",
       },
       "model_info": {
-        "description": f"{model_name} from Groq, Groq is known for its high performance AI chips, bring up 1000 tokens in seconds",
-        "max_tokens": int(context_window),
+        "description": f"{model_name} from {model['owned_by']} hosted by Groq, Groq is known for its high performance AI chips, bring up 1000 tokens in seconds",
+        "max_tokens": context_window,
       }
     }
